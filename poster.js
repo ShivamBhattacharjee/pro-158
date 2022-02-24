3392d9808c159ddb630ab3fddb294c632c5c3ef2
ShivamBhattacharjee <61831108+ShivamBhattacharjee@users.noreply.github.com>
2022-02-24 23:51:28 +0530

    Add files via upload

---
+++ poster.js
@@ -0,0 +1,147 @@
+AFRAME.registerComponent("poster", {
+    init: function () {
+      this.placesContainer = this.el;
+      this.createCards()
+    },
+  
+    createBorder:function(position,id){
+      const entitiyEl = document.createElement("a-entity")
+      entitiyEl.setAttribute("id",id)
+      entitiyEl.setAttribute("visible",true)
+      entitiyEl.setAttribute("geometry",{
+        primitive:"ring",
+        radiusInner:9,
+        radiusOuter:10,
+      })
+      entitiyEl.setAttribute("position",position)
+      entitiyEl.setAttribute("material",{
+        color:"#00bcd4",
+        opacity:0.4,
+      })
+      return entitiyEl
+    },
+  
+    
+    createThumbnail:function(item){
+      const entitiyEl = document.createElement("a-entity")
+      entitiyEl.setAttribute("visible",true)
+      entitiyEl.setAttribute("geometry",{
+        primitive:"circle",
+        radius:9
+      })
+      entitiyEl.setAttribute("material",{
+        src:item.url
+      })
+      return entitiyEl
+    },
+  
+    
+    createTitle:function(position,item){
+      const entitiyEl = document.createElement("a-entity")
+      entitiyEl.setAttribute("text",{
+        font:"exo2bold",aligen:"center",width:60,color:"#e65100",value:item.title 
+      })
+      entitiyEl.setAttribute("visible",true)
+      const elPos=position
+      elPos.y=-20
+      elPos.x=0
+      entitiyEl.setAttribute("position",elPos)
+      return entitiyEl
+    },
+  
+    createCards: function () {
+      const thumbNailsRef = [
+        {
+          id: "spider-man",
+          title: "SpiderMan",
+          url: "./assets/posters/spidey.jpg",
+        },
+        {
+          id: "iron-man",
+          title: "Ironman",
+          url: "./assets/posters/ironman.jpg",
+        },
+  
+        {
+          id: "bat-man",
+          title: "Batman",
+          url: "./assets/posters/batman.jpg",
+        },
+        {
+          id: "black-panther",
+          title: "Black Panther",
+          url: "./assets/posters/b_panther.jpg",
+        },
+      ];
+      let prevoiusXPosition = -60;
+
+    for (var item of thumbNailsRef) {
+      const posX = prevoiusXPosition + 25;
+      const posY = 10;
+      const posZ = -40;
+      const position = { x: posX, y: posY, z: posZ };
+      prevoiusXPosition = posX;
+
+      // Border Element
+      const borderEl = this.createBorder(position, item.id);
+
+      // Thumbnail Element
+      const thumbNail = this.createThumbNail(item);
+      borderEl.appendChild(thumbNail);
+
+      // Title Text Element
+      const titleEl = this.createTitleEl(position, item);
+      borderEl.appendChild(titleEl);
+
+      this.placesContainer.appendChild(borderEl);
+    }
+  },
+  createBorder: function (position, id) {
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("id", id);
+    entityEl.setAttribute("visible", true);
+    entityEl.setAttribute("geometry", {
+      primitive: "ring",
+      radiusInner: 9,
+      radiusOuter: 10,
+    });
+    entityEl.setAttribute("position", position);
+    entityEl.setAttribute("material", {
+      color: "#0077CC",
+      opacity: 1,
+    });
+    
+    entityEl.setAttribute("cursor-listener", {});
+
+    return entityEl;
+  },
+  createThumbNail: function (item) {
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("visible", true);
+    entityEl.setAttribute("geometry", {
+      primitive: "circle",
+      radius: 9,
+    });
+    entityEl.setAttribute("material", { src: item.url });
+
+    return entityEl;
+  },
+  createTitleEl: function (position, item) {
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("text", {
+      font: "exo2bold",
+      align: "center",
+      width: 70,
+      color: "#e65100",
+      value: item.title,
+    });
+    const elPosition = position;
+    elPosition.y = -20;
+    entityEl.setAttribute("position", elPosition);
+    entityEl.setAttribute("visible", true);
+    
+    return entityEl;
+  },
+});
+
+  
